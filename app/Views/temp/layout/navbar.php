919abefd3b67121e444aafd5d350baca4eb08e76
stefanfredik <stefanfredik@gmail.com>
2022-11-22 21:42:39 +0800

    update sidebar

--- app/Views/temp/layout/navbar.php
+++ app/Views/temp/layout/navbar.php
@@ -4,38 +4,40 @@
             <svg class="icon icon-lg">
                 <use xlink:href="/core/vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
             </svg>
-        </button><a class="header-brand d-md-none" href="/core/#">
-            <svg width="118" height="46" alt="CoreUI Logo">
+        </button><a class="header-brand d-md-none" href="/">
+            <!-- <svg width="118" height="46" alt="CoreUI Logo">
                 <use xlink:href="/core/assets/brand/coreui.svg#full"></use>
-            </svg></a>
-        <ul class="header-nav d-none d-md-flex">
-            <li class="nav-item"><a class="nav-link" href="/home"><?= WEBTITLE . ' - ' . session()->get("jabatan"); ?></a></li>
-        </ul>
+            </svg></a> -->
+            <img class="img-fluid sidebar-brand-narrow" width="50" height="50" src="/assets/img/bg.png" alt="">
+            <ul class="header-nav d-none d-md-flex">
+                <li class="nav-item"><a class="nav-link" href="/home"><?= WEBTITLE . ' - ' . session()->get("jabatan"); ?></a></li>
+            </ul>
 
-        <ul class="header-nav ms-3">
-            <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="/#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <div class="avatar avatar-md"><svg class="avatar-img" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end pt-0">
-                    <div class="dropdown-header bg-light py-2">
-                        <div class="fw-semibold">Profile</div>
-                    </div><button class="dropdown-item" href="#">
-                        <svg class="icon me-2">
-                            <use xlink:href="/core/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-                        </svg> <?= session()->get('namaUser'); ?></button><a class="dropdown-item" href="/#">
+            <ul class="header-nav ms-3">
+                <li class="nav-item dropdown">
+                    <a class="nav-link py-0" data-coreui-toggle="dropdown" href="/" role="button" aria-haspopup="true" aria-expanded="false">
+                        <div class="avatar avatar-md"><svg class="avatar-img" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg>
                     </a>
+                    <div class="dropdown-menu dropdown-menu-end pt-0">
+                        <div class="dropdown-header bg-light py-2">
+                            <div class="fw-semibold">Profile</div>
+                        </div><button class="dropdown-item" href="#">
+                            <svg class="icon me-2">
+                                <use xlink:href="/core/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                            </svg> <?= session()->get('namaUser'); ?></button><a class="dropdown-item" href="/#">
+                        </a>
 
-                    <div class="dropdown-divider"></div><a class="dropdown-item" href="/logout">
-                        <svg class="icon me-2">
-                            <use xlink:href="/core/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-                        </svg> Logout
-                    </a>
-                </div>
-            </li>
-        </ul>
+                        <div class="dropdown-divider"></div><a class="dropdown-item" href="/logout">
+                            <svg class="icon me-2">
+                                <use xlink:href="/core/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                            </svg> Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
     </div>
     <div class="header-divider"></div>
     <div class="container-fluid">
--- app/Views/temp/layout/sidebar.php
+++ app/Views/temp/layout/sidebar.php
@@ -1,11 +1,7 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="/core/assets/brand/coreui.svg#full"></use>
-        </svg>
-        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="/core/assets/brand/coreui.svg#signet"></use>
-        </svg>
+        <img class="img-fluid sidebar-brand-full" width="50" height="50" src="/assets/img/bg.png" alt="">
+        <img class="img-fluid sidebar-brand-narrow" width="50" height="50" src="/assets/img/bg.png" alt="">
     </div>
 
     <div class="sidebar-brand d-none d-md-flex">
